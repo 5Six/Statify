85e2a9d9e79c9a6151131679e8a25468f0171a54
Warren Yip <warren8888@hotmail.com>
2024-05-17 01:23:55 +0100

    Add Spotify login

--- backend/statify/wsgi.py
+++ backend/statify/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for api project.
+WSGI config for statify project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'statify.settings')
 
 application = get_wsgi_application()
